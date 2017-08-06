7861ddd97d9f0591bddc92164695dab7dff707e8
nakano taku <kosmo.waizu0804@gmail.com>
2017-08-06 11:34:13 +0900

    10.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -20,8 +22,13 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
     if @user.update_attribute(user_params)
-      # 処理
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -30,5 +37,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in'
+        redirect_to login_url
+      end
     end
 end
